1885a0f79ddadf3c9e64ada62e4081e8b8bbcdf6
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-04-30 22:14:32 +0300

    Create Maven Project

---
+++ sort-app/src/main/java/com/example/sortapp/Main.java
@@ -0,0 +1,13 @@
+package com.example.sortapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
