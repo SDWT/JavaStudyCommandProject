0dc2268c6270c48e6a485f10495a36e5c6207916
mkd <kirilma26@gmail.com>
2026-05-01 13:34:01 +0300

    feat: implement bubble sort

--- sort-app/src/main/java/com/example/sortapp/sorting/algorithm/BubbleSorter.java
+++ sort-app/src/main/java/com/example/sortapp/sorting/algorithm/BubbleSorter.java
@@ -1,5 +1,6 @@
 package com.example.sortapp.sorting.algorithm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.sortapp.model.User;
@@ -13,6 +14,24 @@ public class BubbleSorter extends UserSorter {
 
     @Override
     public List<User> sort(List<User> users) {
-        return null;
+        int size = users.size();
+        var result = new ArrayList<User>(users);
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size - 1; j++) {
+                var userA = users.get(j);
+                var userB = users.get(j + 1);
+                if (comparator.compare(userA, userB) > 0) {
+                    swap(users, j, j + 1);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void swap(List<User> users, int a, int b) {
+        var userA = users.get(a);
+        var userB = users.get(b);
+        users.set(a, userB);
+        users.set(b, userA);
     }
 }
