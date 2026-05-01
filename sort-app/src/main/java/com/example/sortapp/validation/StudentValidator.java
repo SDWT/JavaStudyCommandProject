3cd1a091ed0be2e26053bc345b48d8c842a06dd8
Дмитрий Самсонов <NomekopSD@yandex.ru>
2026-05-01 10:45:41 +0300

    Add StudentValidator for example Student Model

---
+++ sort-app/src/main/java/com/example/sortapp/validation/StudentValidator.java
@@ -0,0 +1,11 @@
+package com.example.sortapp.validation;
+
+// TODO
+// Переименовать класс в соответсвии с класом модели
+public class StudentValidator {
+
+    public static boolean isValid(Object s) {
+        // TODO
+        return true;
+    }
+}
