77ffc38b8f9881d6867668defa9c69aebe8663fd
Artem <Kaktys2025@yandex.ru>
2026-05-08 15:17:24 +0300

    Запись в файл (append) #16

--- sort-app/src/main/java/com/example/sortapp/repository/UserRepository.java
+++ sort-app/src/main/java/com/example/sortapp/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.sortapp.repository;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // TODO
 // Переименовать класс в соответсвии с класом модели
@@ -12,6 +16,28 @@ public class UserRepository {
     }
 
     public void appendToFile(String path, List<Object> students) {
-        // TODO
+        try {
+            List<String> lines = students.stream()
+                    .map(obj -> {
+                        Student s = (Student) obj;
+
+                        String booksInfo = s.getBooks().stream()
+                                .map(b -> b.getTitle() + ":" + b.getYear() + ":" + b.getPages())
+                                .collect(Collectors.joining(";"));
+
+
+                        return s.getName() + ";" + booksInfo;
+                    })
+                    .collect(Collectors.toList());
+
+            Files.write(
+                    Paths.get(path),
+                    lines,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
